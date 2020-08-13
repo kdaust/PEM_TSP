d05c127fc0f10257a01bf5ec8c7a826fb4d4d037
Kiri Daust <kiridaust@gmail.com>
2020-08-13 16:24:40 -0700

    Major Updates, added runs in ESSFmc

--- HeliPoint_TSP.R
+++ HeliPoint_TSP.R
@@ -32,25 +32,26 @@ slopeAdjust <- function(slope){1+((slope-25)*0.02)}
 slope_raster <-  grep("^slope", list.files(datLoc))
 slope <- raster(list.files(datLoc, full.name = TRUE)[slope_raster])
 
-# read in already sampled locations
-included <- st_read(paste0(datLoc,"/ESSF_samples.gpkg"))
-
 ##read in buffer
 buff <- st_read("InputData/ESSF_Buffer.gpkg")
 
 ## clip to just ESSF
-# boundary <- st_read(paste0(datLoc,"/bec_edited.gpkg"))
-# boundary <- boundary[,"MAP_LABEL"]
-# boundary <- boundary[grep("ESSF",boundary$MAP_LABEL),]
-# b2 <- st_union(boundary)
-# b2 <- fasterize(boundary, slope)
+boundary <- st_read(paste0(datLoc,"/bec_edited.gpkg"))
+boundary <- boundary[,"MAP_LABEL"]
+boundary <- boundary[boundary$MAP_LABEL == "ESSFmc",]
+b2 <- st_union(boundary)
+b2 <- fasterize(boundary, slope)
 buff <- fasterize(buff, slope)
+buff <- mask(buff,b2)
 ancDat <- mask(ancDat,buff)
 
 ##read in drop points
-heliDrop <- st_read(paste0(datLoc,"/Deception_Heli_Samples.gpkg"))
+heliDrop <- st_read(paste0(datLoc,"/DropCombined.gpkg"))
 heliDrop <- heliDrop[,"name"]
 heliDrop <- st_transform(heliDrop, 3005)
+#heliDrop <- st_zm(heliDrop)
+heliDrop <- heliDrop[boundary,]
+#heliDrop <- st_transform(heliDrop, st_crs(pnts))
 start <- as(heliDrop, "Spatial")
 
 #slope <- raster(paste0(shapes_path,"/slope.tif"))
@@ -61,111 +62,164 @@ names(cost) <- "cost"
 
 tr <- transition(cost, transitionFunction = function(x) 1/mean(x), directions = 8) 
 
-### unsliced (cost layer includes all drop points)
-acost <- accCost(tr, start)
-lays <- stack(ancDat,acost)
-names(lays) <- c("DAH","LFC","MRVBF","DEM","cost")
-
-s <- sampleRegular(lays , size = 500000, sp = TRUE) # sample raster
-s <- s[!is.na(s$DAH) & !is.infinite(s$cost),]
-s2 <- st_as_sf(s)
-## have to add already sampled points to data
-included <- st_transform(included, st_crs(s))
-incPnts <- raster::extract(lays, included, sp = T)
-incPnts <- incPnts[,-(1:3)]
-incPnts <- st_as_sf(incPnts)
-s <- rbind(incPnts, s2)
-
-### get sample locations
-spoints <- clhs_dist(s,size = 50+nrow(incPnts), minDist = 260, maxCost = 2, include = 1:nrow(incPnts), 
-                  cost = "cost", iter = 5000, simple = F,progress = T)
-############################################################################
-
-pnts <- spoints$sampled_data
-
-plot(acost)
-plot(pnts, add = T)
-
-
-ac2 <- acost
-ac2[ac2 > 2.5] <- 20
-plot(ac2)
-
-p2 <- st_as_sf(pnts)
-heliDrop <- st_zm(heliDrop)
-heliDrop <- st_transform(heliDrop, st_crs(pnts))
-pnts <- pnts[,"DAH"]
-colnames(pnts) <- c("name","geom")
-st_geometry(pnts) <- "geom"
-pnts <- rbind(pnts, heliDrop)
-pnts2 <- as(pnts, "Spatial")
-
-## create distance matrix between sample points
-test <- costDistance(tr,pnts2,pnts2)
-dMat2 <- as.matrix(test)
-dMat2 <- dMat2*60
-dMat2[is.infinite(dMat2)] <- 1000
+# read in already sampled locations
+included <- st_read(paste0(datLoc,"/ESSF_samples.gpkg"))
+included <- st_transform(included, st_crs(ancDat))
 
 source_python("./mTSP.py")
 maxTime <- 8L ##hours
 plotTime <- 45L ##mins
-## note that indexing in python starts at 0, not 1
-## to not allow dropped sites, set penalty > 10000
 
-##penalty
-objVals <- spoints[["final_obj"]][1:50]
-objVals <- objVals - max(objVals)
-minPen <- (maxTime*60L)/2L
-maxPen <- (maxTime*60L)*2L
-objVals <- scales::rescale(objVals, to = c(minPen,maxPen))
-objVals <- as.integer(objVals)
-
-## this one for two routes at each drop
-vrp <- py_mTSP(dat = dMat2,num_days = 20L, start = c(50:59,50:59), 
-               end = c(50:59,50:59), max_cost = maxTime*60L, plot_time = plotTime,penalty =  maxTime*60L+5L)
-
-## one route at each drop
-# dMat2[51:60,] <- 0
-# dMat2[,51:60] <- 0
-vrp <- py_mTSP(dat = dMat2,num_days = 10L, start = 50:59, end = 50:59, 
-               max_cost = maxTime*60L, plot_time = plotTime, penalty =  objVals, arbDepot = F)
-
-result <- vrp[[1]]
-
-## create spatial paths
-paths <- foreach(j = 0:(length(result)-1), .combine = rbind) %do% {
-  if(length(result[[as.character(j)]]) > 2){
-    cat("Drop site",j,"...\n")
-    p1 <- result[[as.character(j)]]+1
-    out <- foreach(i = 1:(length(p1)-1), .combine = rbind) %do% {
-      temp1 <- pnts[p1[i],]
-      temp2 <- pnts[p1[i+1],]
-      temp3 <- shortestPath(tr,st_coordinates(temp1),
-                            st_coordinates(temp2),output = "SpatialLines") %>% st_as_sf()
-      temp3$Segment = i
-      temp3
-    }
-    out$DropSite = j
-    out
-  }
+createLayout <- function(startPnts, toInclude, nPoints){
+  acost <- accCost(tr, startPnts)
+  lays <- stack(ancDat,acost)
+  names(lays) <- c("DAH","LFC","MRVBF","DEM","cost")
   
-}
-filename <- "Heli_ObjPenalty.gpkg"
+  incPnts <- raster::extract(lays, toInclude, sp = T)
+  incPnts <- incPnts[,-(1:3)]
+  incPnts <- st_as_sf(incPnts)
+  
+  s <- sampleRegular(lays , size = 500000, sp = TRUE) # sample raster
+  s <- s[!is.na(s$DAH) & !is.infinite(s$cost),]
+  s2 <- st_as_sf(s)
+  ## have to add already sampled points to data
+  s <- rbind(incPnts, s2)
+  
+  ### get sample locations
+  spoints <- clhs_dist(s,size = nPoints+nrow(toInclude), minDist = 260, maxCost = 2, include = 1:nrow(toInclude), 
+                       cost = "cost", iter = 2000, simple = F,progress = T)
+  ############################################################################
+  
+  pnts <- spoints$sampled_data
+  
+  plot(acost)
+  plot(pnts, add = T)
+  
+  p2 <- st_as_sf(pnts)
+  pnts <- pnts[,"DAH"]
+  colnames(pnts) <- c("name","geometry")
+  st_geometry(pnts) <- "geometry"
+  startPnts <- st_as_sf(startPnts) %>% st_transform(st_crs(pnts))
+  pnts <- rbind(pnts, startPnts)
+  pnts2 <- as(pnts, "Spatial")
+  
+  ## create distance matrix between sample points
+  test <- costDistance(tr,pnts2,pnts2)
+  dMat2 <- as.matrix(test)
+  dMat2 <- dMat2*60
+  dMat2[is.infinite(dMat2)] <- 1000
+  
+  
+  ## note that indexing in python starts at 0, not 1
+  ## to not allow dropped sites, set penalty > 10000
+  
+  ##penalty
+  objVals <- spoints[["final_obj"]][1:nPoints]
+  objVals <- max(objVals) - objVals
+  minPen <- (maxTime*60L)/2L
+  maxPen <- (maxTime*60L)*2L
+  objVals <- scales::rescale(objVals, to = c(minPen,maxPen))
+  objVals <- as.integer(objVals)
 
-st_write(paths, dsn = filename, layer = "Paths", append = T, driver = "GPKG")  
-
-## label points
-p2$PID <- seq_along(p2$DAH)
-p2 <- p2[,"PID"]
-p2$DropLoc <- NA
-p2$Order <- NA
-for(i in 0:(length(result)-1)){
-  p1 <- result[[as.character(i)]]+1
-  p1 <- p1[-c(1,length(p1))]
-  p2$DropLoc[p1] <- i
-  p2$Order[p1] <- 1:(length(p1))
+  # dMat2[51:60,] <- 0
+  # dMat2[,51:60] <- 0
+  ##fixed start, arbitrary end
+  dMat2 <- rbind(dMat2, rep(0, ncol(dMat2)))
+  dMat2 <- cbind(dMat2, rep(0, nrow(dMat2)))
+  indStart <- as.integer(nPoints:(nPoints+nrow(startPnts)-1))
+  indEnd <- as.integer(rep(nrow(dMat2)-1,nrow(startPnts)))
+  vrp <- py_mTSP(dat = dMat2,num_days = 2L, start = indStart, end = indEnd, 
+                 max_cost = maxTime*60L, plot_time = plotTime, penalty =  objVals, arbDepot = T)
+  
+  return(list(route = vrp, objective = spoints$obj[2000],pnts = pnts))
 }
-st_write(p2, dsn = filename,layer = "Points", append = T,overwrite = T, driver = "GPKG")
+
+worker.init <- function(){
+  Rcpp::sourceCpp("CppCLHS.cpp")
+}
+
+require(doParallel)
+cl <- makePSOCKcluster(detectCores()-2)
+clusterCall(cl, worker.init)
+registerDoParallel(cl)
+
+dropInd <- 1:nrow(heliDrop)
+combs <- combn(dropInd, m = 2)
+
+outStats <- foreach(i = 1:ncol(combs), .combine = c, 
+                    .packages = c("Rcpp","reticulate","sf","raster","gdistance"),
+                    .noexport = "c_cor") %dopar% {
+  source_python("./mTSP.py")
+  res <- createLayout(startPnts = start[c(combs[1,i],combs[2,i]),],toInclude = included, nPoints = 10)
+  route <- res$route
+  temp <- data.frame(start = paste(combs[1,i],combs[2,i],sep = "_"),cost = sum(unlist(route[[2]])), 
+             num = paste(length(route[[1]][["0"]]),length(route[[1]][["1"]])), objFun = res$objective)
+  out <- list(list(stats = temp, solution = route,points = res$pnts))
+  names(out) <- i
+  out
+}
+
+stats <- foreach(i = 1:ncol(combs), .combine = rbind) %do% {
+  outStats[[i]][["stats"]]
+}
+stats <- stats[stats$num == "6 6",]
+ids <- rownames(stats[order(stats$cost),])
+
+for(x in 1:length(ids)){
+  writeLayout(id = ids[x], filename = paste0("ESSF_",x,".gpkg"))
+}
+
+writeLayout <- function(id,filename){
+  vrp <- outStats[[id]][["solution"]]
+  pnts <- outStats[[id]][["points"]]
+  result <- vrp[[1]]
+  
+  ## create spatial paths
+  paths <- foreach(j = 0:(length(result)-1), .combine = rbind) %do% {
+    if(length(result[[as.character(j)]]) > 2){
+      cat("Drop site",j,"...\n")
+      p1 <- result[[as.character(j)]]+1
+      out <- foreach(i = 1:(length(p1)-1), .combine = rbind) %do% {
+        temp1 <- pnts[p1[i],]
+        temp2 <- pnts[p1[i+1],]
+        temp3 <- shortestPath(tr,st_coordinates(temp1),
+                              st_coordinates(temp2),output = "SpatialLines") %>% st_as_sf()
+        temp3$Segment = i
+        temp3
+      }
+      out$DropSite = j
+      out
+    }
+    
+  }
+
+  paths <- st_transform(paths, 3005)
+  st_write(paths, dsn = filename, layer = "Paths", append = T, driver = "GPKG")  
+  
+  ## label points
+  p2 <- pnts
+  p2$PID <- seq_along(p2$name)
+  p2 <- p2[,"PID"]
+  p2$DropLoc <- NA
+  p2$Order <- NA
+  for(i in 0:(length(result)-1)){
+    p1 <- result[[as.character(i)]]+1
+    p1 <- p1[-c(1,length(p1))]
+    p2$DropLoc[p1] <- i
+    p2$Order[p1] <- 1:(length(p1))
+  }
+  p2 <- st_transform(p2, 3005)
+  st_write(p2, dsn = filename,layer = "Points", append = T,overwrite = T, driver = "GPKG")
+  return(TRUE)
+}
+
+
+# ## this one for two routes at each drop
+# vrp <- py_mTSP(dat = dMat2,num_days = 20L, start = c(50:59,50:59), 
+#                end = c(50:59,50:59), max_cost = maxTime*60L, plot_time = plotTime,penalty =  maxTime*60L+5L)
+# ### unsliced (cost layer includes all drop points)
+
+
 temp <- mask(acost, buff)
 writeRaster(temp, "CostSurface.tif",format = "GTiff")
 #################################################################
