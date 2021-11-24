3b0aea97dd63a726f2307c50518ece3787e7c177
Kiri Daust <kiridaust@gmail.com>
2021-11-23 17:45:42 -0800

    Bunch of work

--- SampleAnalysis_functions.R
+++ SampleAnalysis_functions.R
@@ -34,19 +34,10 @@ Tri_build <- function(id, x, y){
   return(out)
 } 
 
-KL_stat_clhs <- function(sample){
+KL_stat_clhs <- function(sample,ancSmall){
   sampDat <- as.data.table(sample)
   sampDat <- na.omit(sampDat)
   nsamp <- nrow(sampDat)
-  
-  ancBin <- ancVals[,lapply(.SD, function(x){cut(x,breaks = seq(min(x),max(x),length.out = 9),labels = F,include.lowest = T)})]
-  ancBin <- copy(unite(ancBin, LHSVar, sep = "_"))
-  ancSmall <- ancBin[,.(Num = .N), by = .(LHSVar)] ##count occurences
-  ancSmall <- ancSmall[Num > 150,] ##remove uncommon
-  setorder(ancSmall,-Num)
-  ancSmall[,ID := seq_along(Num)]
-  ancBin[ancSmall, ID := i.ID, on = "LHSVar"]
-  ancBin <- na.omit(ancBin)
   
   fullBreaks <- ancVals[,lapply(.SD,function(x){seq(min(x),max(x),length.out = 9)})]
   ##split into same bins as full data set
@@ -65,29 +56,16 @@ KL_stat_clhs <- function(sample){
   fullDat[,Num := Num/sum(Num)]
   tempMat <- rbind(fullDat$Num,fullDat$SampNum)
   
-  kl2 <- KL(tempMat) ## return KL divergence
-  js2 <- JSD(tempMat) ## returns JS divergenxe
-  temp <- t(tempMat)# %>% dplyr::select()
-  ks2 <- ks.test(temp[,1], temp[,2], alternative = "g")
-  ks2p <- ks2$p.value## returns KS statistic and p value
-  return(kl2, ks2p, js2, js2, ks2$statistic)
+  kl2 <- suppressMessages(KL(tempMat)) ## return KL divergence
+  return(kl2) #, ks2p, js2, js2, ks2$statistic
 }
 
-KS_stat_clhs <- function(sample){
+plotUniDists <- function(sample){
   sampDat <- as.data.table(sample)
   sampDat <- na.omit(sampDat)
   nsamp <- nrow(sampDat)
   
-  ancBin <- ancVals[,lapply(.SD, function(x){cut(x,breaks = seq(min(x),max(x),length.out = 15),labels = F,include.lowest = T)})]
-  ancBin <- copy(unite(ancBin, LHSVar, sep = "_"))
-  ancSmall <- ancBin[,.(Num = .N), by = .(LHSVar)] ##count occurences
-  ancSmall <- ancSmall[Num > 150,] ##remove uncommon
-  setorder(ancSmall,-Num)
-  ancSmall[,ID := seq_along(Num)]
-  ancBin[ancSmall, ID := i.ID, on = "LHSVar"]
-  ancBin <- na.omit(ancBin)
-  
-  fullBreaks <- ancVals[,lapply(.SD,function(x){seq(min(x),max(x),length.out = 15)})]
+  fullBreaks <- ancVals[,lapply(.SD,function(x){seq(min(x),max(x),length.out = 9)})]
   ##split into same bins as full data set
   for(i in 1:ncol(sampDat)){
     sampDat[[i]] <- cut(sampDat[[i]],breaks = fullBreaks[[i]], labels = F, include.lowest = T)
@@ -95,7 +73,6 @@ KS_stat_clhs <- function(sample){
   sampBin <- copy(unite(sampDat,sampLHS,sep = "_"))
   sampBin[ancSmall, ID := i.ID, on = c(sampLHS = "LHSVar")]
   sampBin <- na.omit(sampBin)
-  
   sampBin <- sampBin[,.(SampNum = .N), by = .(sampLHS)]##count
   #merge to compare
   fullDat <- copy(ancSmall)
@@ -103,12 +80,47 @@ KS_stat_clhs <- function(sample){
   fullDat[is.na(SampNum), SampNum := 0]
   fullDat[,SampNum := SampNum/sum(SampNum)]
   fullDat[,Num := Num/sum(Num)]
-  #fullDat[,Diff := abs(Num - SampNum)]
-  t1 <- cumsum(fullDat$SampNum)
-  t2 <- cumsum(fullDat$Num)
-  return(max(abs(t2-t1)))
-  #return(mean(fullDat$Diff))
+  tempMat <- rbind(fullDat$Num,fullDat$SampNum)
+  kl2 <- KL(tempMat)
+  return(list(data = fullDat, KL = kl2))
+  
 }
+
+Tri_build <- function(id, x, y){
+  tris <- CreateRegularPolygon(3, c(x,y), 145) # number of sides, center pt and length of sides
+  tris <- tris[c(1:3,1),]
+  lines <- st_linestring(tris)
+  
+  g = st_sfc(lines)
+  out <- st_sf(g,ID = id)
+  #st_set_crs(newproj)
+  return(out)
+} 
+
+rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
+
+## Feature rotation
+rotFeature <- function(Feature, PivotPt, Bearing) {
+  # where Feature is the Shape to be rotated, eg:  #Feature <- tri
+  # Bearing is the compass bearing to rotate to    #PivotPt <- pt.sf
+  # PivotPt is the point to rotate around          #Bearing <- 15 
+  
+  ## extract the geometry
+  Feature_geo <- st_geometry(Feature)
+  PivotPoint  <- st_geometry(PivotPt)
+  
+  ## Convert bearing from degrees to radians
+  d <- ifelse(Bearing > 180, pi * ((Bearing-360)/ 180) ,  pi * (Bearing / 180))
+  
+  rFeature <- (Feature_geo - PivotPoint) * rot(d)   + PivotPoint
+  rFeature <- st_set_crs(rFeature, st_crs(Feature))
+  
+  Feature$geometry <- st_geometry(rFeature) ## replace the original geometry
+  return(Feature)
+}
+
+
+
 #sample = s1
 plotUniDists <- function(sample){
   sampDat <- as.data.table(sample)
